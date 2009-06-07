2e6ffabf567a290799a14f461e746f8af96e178c
Cihan Altinay <c.altinay@uq.edu.au>
2009-06-07 03:29:32 +0000

    VRL: Added combobox to WallLoader and fixed script output adding quotes to string.

--- src/main/webapp/js/ScriptBuilder/components/WallLoader.js
+++ src/main/webapp/js/ScriptBuilder/components/WallLoader.js
@@ -9,10 +9,20 @@ WallLoaderNode = Ext.extend(ScriptBuilder.BaseComponent, {
     );
   },
 
+  fillFormValues: function(form) {
+    form.setValues(this.values);
+    var wallList = this.container.getWalls();
+    var store = form.findField('wallName').getStore();
+    store.removeAll();
+    for (var i=0; i<wallList.length; i++) {
+        store.add(new store.recordType({'text': wallList[i].getUniqueName()}));
+    }
+  },
+
   getScript: function() {
     var ret = "from WallLoader import WallLoaderRunnable\n\n";
     ret+=this.values.uniqueName+" = WallLoaderRunnable (\n";
-    ret+="   LsmMpi = sim,\n   wallName = "+this.values.wallName+",\n";
+    ret+="   LsmMpi = sim,\n   wallName = \""+this.values.wallName+"\",\n";
     ret+="   vPlate = Vec3("+this.values.plateX+", "+this.values.plateY+", "+this.values.plateZ+"),\n";
     ret+="   dt = "+this.container.getValues().timeIncrement+"\n)\n";
     if (this.values.snapTiming == 0) {
